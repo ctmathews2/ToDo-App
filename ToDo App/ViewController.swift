bbfcb5949b65fc0aec95d8b73533a3c2453a9a79
Chandler Mathews <ctmathews2@usfca.edu>
2020-05-16 12:02:29 -0700

    Added UITable View

--- ToDo App/ViewController.swift
+++ ToDo App/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var centerText: UILabel!
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    private var data: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-    @IBAction func changeColor(_ sender: UIButton) {
-        centerText.text = "Good bye world!"
+        for i in 0...100{
+            data.append("\(i)")
+        }
+        tableView.dataSource = self
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")! //1.
+        
+        let text = data[indexPath.row] //2.
+        
+        cell.textLabel?.text = text //3.
+        
+        return cell //4.
+    }
+
 }
 
