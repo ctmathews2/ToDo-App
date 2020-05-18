8275aa167f076602999fe5737ca6ad945019e858
Chandler Mathews <ctmathews2@usfca.edu>
2020-05-18 09:58:10 -0700

    Added button to add to list

--- ToDo App/ViewController.swift
+++ ToDo App/ViewController.swift
@@ -2,9 +2,11 @@ import UIKit
 class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
-    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
+    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
     
+    @IBOutlet weak var addButton: UIButton!
     
+    @IBOutlet weak var testCollectionView: UICollectionView!
     // MARK: - UICollectionViewDataSource protocol
     
     // tell the collection view how many cells to make
@@ -26,6 +28,14 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         cell.layer.cornerRadius = 8
         
         return cell
+    }
+    
+    @IBAction func addToList(_ sender: UIButton) {
+            let totalAmount = items.count+1
+            items.append("\(totalAmount)")
+            let indexPath = IndexPath(row: items.count - 1, section: 0)
+            testCollectionView.insertItems(at: [indexPath])
+            print("This is the right button")
     }
     
     // MARK: - UICollectionViewDelegate protocol
