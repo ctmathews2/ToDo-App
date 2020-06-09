9e47bbe0c3aac5fb57dbb5862015650e79977cb4
Chandler Mathews <ctmathews2@usfca.edu>
2020-06-09 09:54:32 -0700

    installed Firebase

---
+++ ToDo App/RegisterVC.swift
@@ -0,0 +1,37 @@
+//
+//  RegisterVC.swift
+//  ToDo App
+//
+//  Created by Chandler Mathews on 6/9/20.
+//  Copyright © 2020 Chandler Mathews. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class RegisterVC: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
